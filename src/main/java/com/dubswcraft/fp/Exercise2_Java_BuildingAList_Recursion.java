26e0353739c3c1be02c651b4a3a117c22d74e537
Paul Kelly <pkelly@equalexperts.com>
2016-06-15 22:38:42 +0100

    some additions to the exercises

--- src/main/java/com/dubswcraft/fp/Exercise2_Java_BuildingAList_Recursion.java
+++
@@ -1,8 +0,0 @@
-package com.dubswcraft.fp;
-
-public class Exercise2_Java_BuildingAList_Recursion {
-
-    public static void main(String[] args) {
-    }
-
-}
--- src/main/scala/com/dubswcraft/fp/FPExercises.scala
+++ src/main/scala/com/dubswcraft/fp/FPExercises.scala
@@ -1,9 +1,31 @@
 package com.dubswcraft.fp
 
+/*
+
+//Evaluates a function over each element in the list, returning a list with the same number of elements.
+
+def byTwo(numbers: List[Int]) = numbers.map(_ * 2)
+
+byTwo(List(1,2,3))
+ */
 object Exercise1_Scala_BuildingAList_Map {
   def processList(items: List[String]) = items.map(item => item + " processed")
 }
 
+/*
+def sum(xs: List[Int]): Int = {
+  if (xs.isEmpty) 0
+  else xs.head + sum(xs.tail)
+}
+
+def sumTail(xs: List[Int]): Int = {
+  def go(xs: List[Int], count: Int): Int = xs match {
+    case Nil => count
+    case h :: t => go(t, h + count)
+  }
+  go(xs, 0)
+}
+ */
 object Exercise2_Scala_BuildingAList_Recursion {
 
   def processList(items: List[String]): List[String] = {
@@ -15,6 +37,14 @@ object Exercise2_Scala_BuildingAList_Recursion {
   }
 }
 
+/*
+// write our own contains method
+
+def contains(items: List[Int], searchItem: Int) =
+  items.foldLeft(false){(prev, cur) => prev || cur == searchItem }
+
+contains(List(1,2,3), 4)
+ */
 object Exercise3_Scala_MoreAggregating_NoState_Fold {
   case class Person(name: String, height: Int)
 
@@ -24,6 +54,7 @@ object Exercise3_Scala_MoreAggregating_NoState_Fold {
   }
 }
 
+
 object Exercise4_Scala_MoreRecursion {
   def isPalindrome[A](l: List[A]):Boolean = l match {
     case Nil => true
@@ -32,12 +63,38 @@ object Exercise4_Scala_MoreRecursion {
   }
 }
 
+/*
+// like a combination of map with flatten
+val nestedNumbers = List(List(1, 2), List(3, 4))
+nestedNumbers.flatMap(x => x.map(_ * 3))
+
+// what would it look like if i just mapped?
+nestedNumbers.map(x => x.map(_ * 3))
+
+// now I add a flatten on the end
+nestedNumbers.map(x => x.map(_ * 3)).flatten
+*/
+
 object Exercise5_Scala_FlatMapping {
   def duplicate(l:List[Integer]):List[Integer] = {
     l flatMap { e => List(e, e) }
   }
 }
 
+/*
+
+val asXml : String => String = field => "<xml-field>" + field + "</xml-field>"
+val asJson : String => String = field => s"'json-field':'$field'"
+
+type OutputFormat = String => String
+
+def generateReport(formatter: OutputFormat, fieldContents: String) =
+  formatter(fieldContents)
+
+generateReport(asJson, "sample content")
+generateReport(asXml, "sample content")
+
+ */
 object Exercise6_Scala_HigherOrderFunctions {
   type CommissionCalculator = (Int) => BigDecimal
 
